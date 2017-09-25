d51647b723387e5e904ecc7b0f7206ec532f354b
Jared <jrddunbr@gmail.com>
2017-09-25 14:05:08 -0400

    added more tests?

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all: run
 run: clean
 	/usr/bin/python3 play.py
+	/usr/bin/python3 read.py
 clean:
 	rm -f example.db
--- read.py
+++ read.py
@@ -3,14 +3,12 @@
 # Useful stuff:
 # https://docs.python.org/3.6/library/sqlite3.html
 
-print("### begin play.py ###")
+print("### begin read.py ###")
 
 import sqlite3
 
 conn = sqlite3.connect('example.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE test (thing text, that text)''')
-c.execute('''INSERT INTO test VALUES ('thing', 'thing2')''')
 
 for row in c.execute('SELECT * FROM test'):
     print (row)
@@ -18,4 +16,4 @@ for row in c.execute('SELECT * FROM test'):
 conn.commit()
 conn.close()
 
-print("### end play.py ###")
+print("### end read.py ###")
